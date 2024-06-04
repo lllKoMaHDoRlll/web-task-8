979a571b1330226b5688291448569719b7ce6f0f
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-06-04 15:23:36 +0300

    remove redundant lines

--- modules/form_front.php
+++ modules/form_front.php
@@ -3,7 +3,6 @@
 include_once("scripts/utils.php");
 
 function form_front_get($request, $user_id="-1") {
-    echo $user_id;
     $values = parse_submission_from_cookies();
     return theme('form', ['values' => $values]);
 }
