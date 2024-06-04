988d12ca8c6372e8476f3b1313c2106f07ef5078
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-06-04 12:22:45 +0300

    update page template head tags

--- theme/page.tpl.php
+++ theme/page.tpl.php
@@ -1,4 +1,13 @@
-<link rel="stylesheet" href="/styles/page.css">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles/page.css">
+    <script src="./script.js" defer></script>
+</head>
+
 
 <?php
 foreach ($c['#content'] as $content) {
