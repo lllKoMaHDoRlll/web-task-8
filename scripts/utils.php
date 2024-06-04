90ed09442411781b7523df76ca3fc783b0a5a2be
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-06-04 15:20:21 +0300

    update setcookie calls with path: /

--- scripts/utils.php
+++ scripts/utils.php
@@ -38,81 +38,81 @@ function validate_fields_and_set_cookies($values)
     $validation_passed = True;
 
     if (empty($values["name"]) || strlen($values["name"]) > 150 || !preg_match("/^[\p{Cyrillic}a-zA-Z-' ]*$/u", $values["name"])) {
-        setcookie("field-name-error", "1", $expiration_time_on_error);
-        setcookie('field-name', $values["name"], $expiration_time_on_error);
+        setcookie("field-name-error", "1", $expiration_time_on_error, "/");
+        setcookie('field-name', $values["name"], $expiration_time_on_error, "/");
         $validation_passed = False;
     }
     else {
-        setcookie('field-name', $values["name"], $expiration_time_on_success);
-        setcookie("field-name-error", "", 1);
+        setcookie('field-name', $values["name"], $expiration_time_on_success, "/");
+        setcookie("field-name-error", "", 1, "/");
     }
     
     if (empty($values["phone"]) || !preg_match('/[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/i', $values["phone"])) {
-        setcookie("field-phone-error", "1", $expiration_time_on_error);
-        setcookie('field-phone', $values["phone"], $expiration_time_on_error);
+        setcookie("field-phone-error", "1", $expiration_time_on_error, "/");
+        setcookie('field-phone', $values["phone"], $expiration_time_on_error, "/");
         $validation_passed = False;
     }
     else {
-        setcookie('field-phone', $values["phone"], $expiration_time_on_success);
-        setcookie("field-phone-error", "", 1);
+        setcookie('field-phone', $values["phone"], $expiration_time_on_success, "/");
+        setcookie("field-phone-error", "", 1, "/");
     }
 
     if (empty($values["email"]) || !filter_var($values["email"], FILTER_VALIDATE_EMAIL)) {
-        setcookie("field-email-error", "1", $expiration_time_on_error);
-        setcookie('field-email', $values["email"], $expiration_time_on_error);
+        setcookie("field-email-error", "1", $expiration_time_on_error, "/");
+        setcookie('field-email', $values["email"], $expiration_time_on_error, "/");
         $validation_passed = False;
     }
     else {
-        setcookie('field-email', $values["email"], $expiration_time_on_success);
-        setcookie("field-email-error", "", 1);
+        setcookie('field-email', $values["email"], $expiration_time_on_success, "/");
+        setcookie("field-email-error", "", 1, "/");
     }
 
     if (empty($values["date"]) || !preg_match('/^\d{4}-\d{2}-\d{2}$/i', $values["date"])) {
-        setcookie("field-date-error", "1", $expiration_time_on_error);
-        setcookie('field-date', $values["date"], $expiration_time_on_error);
+        setcookie("field-date-error", "1", $expiration_time_on_error, "/");
+        setcookie('field-date', $values["date"], $expiration_time_on_error, "/");
         $validation_passed = False;
     }
     else {
-        setcookie('field-date', $values["date"], $expiration_time_on_success);
-        setcookie("field-date-error", "", 1);
+        setcookie('field-date', $values["date"], $expiration_time_on_success, "/");
+        setcookie("field-date-error", "", 1, "/");
     }
 
     if (empty($values["gender"]) || !preg_match('/^\Qmale\E|\Qfemale\E$/i', $values["gender"])) {
-        setcookie("field-gender-error", "1", $expiration_time_on_error);
-        setcookie('field-gender', $values["gender"], $expiration_time_on_error);
+        setcookie("field-gender-error", "1", $expiration_time_on_error, "/");
+        setcookie('field-gender', $values["gender"], $expiration_time_on_error, "/");
         $validation_passed = False;
     }
     else {
-        setcookie('field-gender', $values["gender"], $expiration_time_on_success);
-        setcookie("field-gender-error", "", 1);
+        setcookie('field-gender', $values["gender"], $expiration_time_on_success, "/");
+        setcookie("field-gender-error", "", 1, "/");
     }
 
     if (empty($values["fpls"]) || count($values["fpls"]) < 1 || !preg_match('/^((\Qpascal\E|\Qc\E|\Qcpp\E|\Qjs\E|\Qphp\E|\Qpython\E|\Qjava\E|\Qhaskel\E|\Qclojure\E|\Qprolog\E|\Qscala\E){1}[\,]{0,1})+$/i', implode(",", $values["fpls"]))) {
-        setcookie("field-pl-error", "1", $expiration_time_on_error);
-        setcookie('field-pl', sprintf("@%s@", implode("@", $values["fpls"])), $expiration_time_on_error);
+        setcookie("field-pl-error", "1", $expiration_time_on_error, "/");
+        setcookie('field-pl', sprintf("@%s@", implode("@", $values["fpls"])), $expiration_time_on_error, "/");
         $validation_passed = False;
     }
     else {
-        setcookie('field-pl', sprintf("@%s@", implode("@", $values["fpls"])), $expiration_time_on_success);
-        setcookie("field-pl-error", "", 1);
+        setcookie('field-pl', sprintf("@%s@", implode("@", $values["fpls"])), $expiration_time_on_success, "/");
+        setcookie("field-pl-error", "", 1, "/");
     }
 
     if (empty($values["acception"]) || $values["acception"] != "accepted") {
-        setcookie("field-accept-error", "1", $expiration_time_on_error);
+        setcookie("field-accept-error", "1", $expiration_time_on_error, "/");
         $validation_passed = False;
     }
     else {
-        setcookie("field-accept-error", "", 1);
+        setcookie("field-accept-error", "", 1, "/");
     }
 
     if (strlen($values["bio"]) > 300) {
-        setcookie("field-bio-error", "1", $expiration_time_on_error);
-        setcookie('field-bio', $values["bio"], $expiration_time_on_error);
+        setcookie("field-bio-error", "1", $expiration_time_on_error, "/");
+        setcookie('field-bio', $values["bio"], $expiration_time_on_error, "/");
         $validation_passed = False;
     }
     else {
-        setcookie('field-bio', $values["bio"], $expiration_time_on_success);
-        setcookie("field-bio-error", "", 1);
+        setcookie('field-bio', $values["bio"], $expiration_time_on_success, "/");
+        setcookie("field-bio-error", "", 1, "/");
     }
 
     return $validation_passed;
