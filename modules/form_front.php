17b5dba0871fbcbc775d405cf84bb6b06758cce2
Aleksandr Orlichenya <aleksandr.orlichenya@gmail.com>
2024-06-05 13:57:34 +0300

    implement checking on user_id in form_front module

--- modules/form_front.php
+++ modules/form_front.php
@@ -3,10 +3,10 @@
 include_once("scripts/utils.php");
 
 function form_front_get($request, $user_id="-1") {
-    if ($user_id !== $_SESSION['user_id']) {
+    if ($user_id !== "-1" && intval($user_id) !== $_SESSION['user_id']) {
         return access_denied();
     }
-    
+
     $values = parse_submission_from_cookies();
     return theme('form', ['values' => $values]);
 }
