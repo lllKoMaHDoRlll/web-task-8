a16be7ace9df751ad96bd39fc0baaf02c33a8b3e
Koma <aleksandr.orlichenya@gmail.com>
2024-06-08 12:25:19 +0300

    implement sendFormSubmission function

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+
+document.cookie = "is-js-enabled=1; path=/;";
+
+function sendSubmission (ev) {
+    ev.preventDefault();
+    const formEl = document.getElementById("submission-form");
+    const fields = new FormData(formEl);
+    
+    fields.entries().forEach(element => {
+        console.log(element);
+    });
+    
+    const request = new XMLHttpRequest();
+
+    let method = "POST";
+    let userIdPath = "";
+    if (fields.get("user_id")) {
+        method = "PUT";
+        userIdPath = "/" + fields.get("user_id");
+    }
+
+    const url = "/api/form" + userIdPath + "?js=1";
+    request.open(method, url);
+    request.setRequestHeader("ACCEPT", "application/json");
+
+    request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            console.log(request);
+        }
+    };
+
+    request.send(fields);
+}
+
+function isFormFieldsValid () {
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    submissionFormEl = document.getElementById("submission-form");
+    if (submissionFormEl) {
+        submissionFormEl.addEventListener('submit', sendSubmission);
+    }
+});
